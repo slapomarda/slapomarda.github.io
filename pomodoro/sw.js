754c669c2576c4077894bf22b4b6f43eef3f5154
slapomarda <simolapomarda@gmail.com>
2026-08-29 16:00:40 +0200

    feat: aggiornato logo pomodoro

--- pomodoro/sw.js
+++ pomodoro/sw.js
@@ -6,7 +6,7 @@ self.addEventListener('push', function(event) {
     event.waitUntil(
         self.registration.showNotification(title, {
             body: body,
-            icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🍅</text></svg>',
+            icon: 'logo.jpg',
             vibrate: [200, 100, 200]
         })
     );
